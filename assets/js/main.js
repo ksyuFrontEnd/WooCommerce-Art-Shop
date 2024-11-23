a8a0ffaf85405bbaa8e3de5f5b00945d7d16f62e
Oksana <ksyubaranik@gmail.com>
2024-11-23 17:11:00 +0100

    Allow qty in grouped product changed to zero

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,13 +22,18 @@ jQuery(document).ready(function($) {
     // Change quantity by click on + and -
     $('.quantity button').on('click', function() {
         let btn = $(this);
+        let groupedProduct = btn.closest('.woocommerce-grouped-product-list-item__quantity').length;
+        console.log(groupedProduct);
+        
         let inputQty = btn.parent().find('.qty');
         let prevValue = +(inputQty.val());
-        let newValue = 1;
+        let newValue = groupedProduct ? 0 : 1;
        if( btn.hasClass( 'btn-plus' )) {
             newValue = prevValue + 1;
        } else {
-            if( prevValue > 1 ) {
+            if (!groupedProduct && prevValue > 1) {
+                newValue = prevValue - 1;
+            } else if (groupedProduct && prevValue > 0) {
                 newValue = prevValue - 1;
             }
        }
