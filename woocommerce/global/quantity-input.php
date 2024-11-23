4dbaae9fec36cc1706c63cb91a37fc3c332f7cb7
Oksana <ksyubaranik@gmail.com>
2024-11-23 16:48:50 +0100

    Display input group only if product qty is bigger than 1

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -1,29 +1,14 @@
 <?php
-/**
- * Product quantity inputs
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/global/quantity-input.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 9.4.0
- *
- * @var bool   $readonly If the input should be set to readonly mode.
- * @var string $type     The input type attribute.
- */
 
 defined( 'ABSPATH' ) || exit;
 
 /* translators: %s: Quantity. */
 $label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 'woocommerce' ), wp_strip_all_tags( $args['product_name'] ) ) : esc_html__( 'Quantity', 'woocommerce' );
 
-?>
+global $product;
+
+if ( $product->get_stock_quantity() > 1 ) : ?>
+
 <div class="quantity">
 	<?php
 	/**
@@ -69,4 +54,5 @@ $label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 
 	do_action( 'woocommerce_after_quantity_input_field' );
 	?>
 </div>
-<?php
+
+<?php endif; ?>
