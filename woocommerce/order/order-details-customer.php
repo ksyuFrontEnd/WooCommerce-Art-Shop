b341fa7fcd668e60f873f64c1c8779443d3e7350
Oksana <ksyubaranik@gmail.com>
2025-02-19 20:26:02 +0100

    Added styles to order details in my-account

--- woocommerce/order/order-details-customer.php
+++ woocommerce/order/order-details-customer.php
@@ -19,6 +19,7 @@ defined( 'ABSPATH' ) || exit;
 
 $show_shipping = ! wc_ship_to_billing_address_only() && $order->needs_shipping_address();
 ?>
+
 <section class="woocommerce-customer-details">
 
 	<?php if ( $show_shipping ) : ?>
