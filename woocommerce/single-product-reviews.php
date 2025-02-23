c091622edbb796f4d694d839b578f7934f05dc1f
Oksana <ksyubaranik@gmail.com>
2025-02-23 20:27:33 +0100

    Added hovers, fixed inputs

--- woocommerce/single-product-reviews.php
+++ woocommerce/single-product-reviews.php
@@ -57,7 +57,9 @@ if ( ! comments_open() ) {
 				$commenter    = wp_get_current_commenter();
 				$comment_form = array(
 					/* translators: %s is product title */
-					'title_reply'         => have_comments() ? esc_html__( 'Add a review', 'woocommerce' ) : sprintf( esc_html__( 'Be the first to review &ldquo;%s&rdquo;', 'woocommerce' ), get_the_title() ),
+					'title_reply' => have_comments() 
+						? esc_html__( 'Add a review', 'woocommerce' ) 
+						: sprintf( esc_html__( 'Будьте першим, хто залишив відгук %s', 'roxydev' ), get_the_title() ),
 					/* translators: %s is product title */
 					'title_reply_to'      => esc_html__( 'Leave a Reply to %s', 'woocommerce' ),
 					'title_reply_before'  => '<span id="reply-title" class="comment-reply-title">',
